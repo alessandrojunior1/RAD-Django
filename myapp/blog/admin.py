c7afb53cbc705db5654b9a551d2c5278c55c419e
Alessandro Rodrigues <alessandjr0@gmail.com>
2025-10-15 21:31:16 -0300

    prática 04 - models

--- myapp/blog/admin.py
+++ myapp/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Editora, Autor, Livro, Publica
+
+admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
+admin.site.register(Publica)
--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Editora(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Editora'
+        verbose_name_plural = 'Editoras'
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+class Livro(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    isbn = models.CharField(max_length=13, unique=True)
+    publicacao = models.DateField()
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    estoque = models.IntegerField()
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    autores = models.ManyToManyField(Autor, through='Publica')
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Livro'
+        verbose_name_plural = 'Livros'
+
+class Publica(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Publicação'
+        verbose_name_plural = 'Publicações'
+        unique_together = ['livro', 'autor']  # Garante que não haja duplicatas de livro-autor
